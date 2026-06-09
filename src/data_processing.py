bc6ba372d16088d8c8caedf3252f9d95490a5944
lucapetrarca <lucapetrarca1@gmail.com>
2026-06-09 03:46:54 -0300

    Data processing modificado para samplear por epoch

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,41 +1,37 @@
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 import os
 
-def procesar_y_guardar_datos(ruta_raw, ruta_processed, ruta_toy, num_muestras=5000):
-    #Cargamos datos crudos
+def procesar_y_guardar_datos(ruta_raw, ruta_processed, ruta_toy):
     df = pd.read_csv(ruta_raw)
 
-    #Nos quedamos con las columnas que nos importan: x, y espaciales; U_x, U_y velocidades; nut viscosidad; implicit_distance al ala
     cols = ['x', 'y', 'U_x', 'U_y', 'nut', 'implicit_distance']
     df = df[cols].copy()
 
-    #Nuestra condición de borde es la superficie del ala
-    #Elegimos los puntos que están casi pegados a la chapa (tolerancia cercana a 0) como el borde, y el resto en el interior del fluido
+    #Separamos las condiciones de borde, implicit distance cercano a 0
     tol = 1e-4
-    df_borde = df[df['implicit_distance'].abs() < tol].copy()
-    df_interior = df[df['implicit_distance'].abs() >= tol].copy()
+    #Creamos columna booleana para saber qué puntos pertenecen al ala
+    df['es_borde'] = df['implicit_distance'].abs() < tol 
 
-    #Para no calcular demasiadas derivadas, submuestreamos el interior (tenía 140000 puntos) y el borde (el borde tenía 20000 puntos)
-    num_interior = num_muestras
-    num_borde = num_muestras
-    df_borde = df_borde.sample(n=min(len(df_borde), num_borde), random_state=42)
-    df_interior = df_interior.sample(n=min(len(df_interior), num_interior), random_state=42) #Ver de nuevo el método de muestreo
-
-    #Nos quedamos con la muestra de 5000 puntos y con las condiciones de borde
-    df_final = pd.concat([df_borde, df_interior], ignore_index=True)
-
-    #Normalizamos a [-1, 1] usando MinMax Scaler
+    #Escalamos a [-1, 1] todo el dataset
     scaler = MinMaxScaler(feature_range=(-1, 1))
     cols_a_escalar = ['x', 'y', 'U_x', 'U_y']
-    df_final[cols_a_escalar] = scaler.fit_transform(df_final[cols_a_escalar])
+    df[cols_a_escalar] = scaler.fit_transform(df[cols_a_escalar])
 
-    #Guardamos el dataset procesado
-    df_final.to_csv(ruta_processed, index=False)
+    df.to_csv(ruta_processed, index=False)
 
-    #Hacemos y guardamos un dataset de "juguete" para realizar pruebas->Son 100 condiciones de borde (las que entran primero)
-    df_toy = df_final.head(100)
+    #Para probar con datos toy, asignamos 250 muestras del ala y 250 del interior
+    df_borde_toy = df[df['es_borde'] == True].sample(n=250, random_state=42)
+    df_interior_toy = df[df['es_borde'] == False].sample(n=250, random_state=42)
+
+    #Los unimos
+    df_toy = pd.concat([df_borde_toy, df_interior_toy], ignore_index=True)
+
+    #Mezclamos los datos toy
+    df_toy = df_toy.sample(frac=1, random_state=42).reset_index(drop=True)
+
+    #Lo guardamos
     df_toy.to_csv(ruta_toy, index=False)
 
-    return df_final
+    return df
